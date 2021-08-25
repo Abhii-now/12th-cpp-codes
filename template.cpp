83fc585d492a2a7fe26e318f98764cf215ce1a51
Abhinav Jain <36236202+Abhii-now@users.noreply.github.com>
2021-08-25 13:13:17 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+//=======================
+const int MOD = 1'000'000'007;
+const int N = 2e6 + 13, M = N;
+const ll inf = 1e16;
+//=======================
+vi g[N];
+int a[N];
+//=======================
+
+void solve()
+{
+    ll n, j, i, x, k;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    ll n, x;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
